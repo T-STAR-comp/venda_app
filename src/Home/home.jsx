f3c633b87626dd76f5b68fc16c400dba9b16e323
unknown <ticketmalawi@gmail.com>
2025-06-30 21:41:16 +0200

    comm

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -564,7 +564,7 @@ const HomeComp = () => {
       {/* Post Ad Modal */}
       <PostAdModal open={showPostAd} onClose={() => setShowPostAd(false)} onSubmit={handlePostAd} />
 
-      {/* Footer uni */}
+      {/* Footer  */}
       <Footer />
     </div>
   );
